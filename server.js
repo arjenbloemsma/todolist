735396fd3aa32cea2ff523d64bae45717e3384bb
arjenbloemsma <Arjen@MacBook-Air-van-Arjen.local>
2016-02-25 21:58:17 +0100

    Added another fine line

--- server.js
+++ server.js
@@ -2,5 +2,5 @@ var http = require('http')
 var port = process.env.PORT || 1337;
 http.createServer(function(req, res) {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Konishiwa Ninja! :-)\n');
+  res.end('Konishiwa Ninja! :-)\nEnjoy some sushi!');
 }).listen(port);
